720c5223d5cbf6b686693b64569cff01c272e86f
SamEThibault <sam5thibault@gmail.com>
2023-03-21 12:35:08 -0400

    idk

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import numpy as np
 import h5py
 
+# Create HDF5 File
 with h5py.File("./data.h5", "w") as hdf:
 
     hdf.create_group("Vivian") # to create a group or "folder"
@@ -8,9 +9,15 @@ with h5py.File("./data.h5", "w") as hdf:
     hdf.create_group("John")
     hdf.create_group("dataset")
 
-    items = list(hdf.items())
-    print(items)
+vivian_group, sam_group, john_group = None
 
-    # hdf.create_dataset(
-    #     "dataset1", data=matrix
-    # )  # where matrix is a np.random.random() object
+with h5py.File("./data.h5", "r") as hdf:
+    vivian_group = hdf["Vivian"]
+    sam_group = hdf["Sam"]
+    john_group = hdf["John"]
+
+
+with h5py.File("./data.h5", "w") as hdf:
+    
+    # John's Datasets Generation 
+    hdf.create_dataset("back_right_J", data=)  # where matrix is a np.random.random() object

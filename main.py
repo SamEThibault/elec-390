a883dfb940a578bf7e99c8188ca053ac45fcc9d7
SamEThibault <sam5thibault@gmail.com>
2023-03-21 12:30:03 -0400

    organizing

--- main.py
+++ main.py
@@ -2,7 +2,15 @@ import numpy as np
 import h5py
 
 with h5py.File("./data.h5", "w") as hdf:
-    hdf.create_group() # to create a group or "folder"
-    hdf.create_dataset(
-        "dataset1", data=matrix
-    )  # where matrix is a np.random.random() object
+
+    hdf.create_group("Vivian") # to create a group or "folder"
+    hdf.create_group("Sam")
+    hdf.create_group("John")
+    hdf.create_group("dataset")
+
+    items = list(hdf.items())
+    print(items)
+
+    # hdf.create_dataset(
+    #     "dataset1", data=matrix
+    # )  # where matrix is a np.random.random() object

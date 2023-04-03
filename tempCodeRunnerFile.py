57f95d3cee6db9b0cfc9cb0450821e17fb09a761
SamEThibault <sam5thibault@gmail.com>
2023-04-03 14:49:06 -0400

    create 2 large dataframes for jumping and walking

---
+++ tempCodeRunnerFile.py
@@ -0,0 +1,5 @@
+    # for title, data in john_group.items():
+    #     if "W" in title:
+    #         jump = pd.concat([jump, pd.DataFrame(data)])
+    #     elif "J" in title:
+    #         walk = pd.concat([walk, pd.DataFrame(data)])
